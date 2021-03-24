5bca3d8d482dca5f38a61e79904b9ba5b816c748
dmoor5 <36045753+dmoor5@users.noreply.github.com>
2021-03-24 18:43:21 -0400

    Create badfood2.js

---
+++ badfood2.js
@@ -0,0 +1,27 @@
+let badFood;
+let badFoodX;
+class BadFood{
+  
+  constructor(){
+    this.x = x;
+    this.y = y;
+    
+  }
+  
+  location(){
+    
+    let x = floor(random(w));
+  let y = floor(random(h));
+    let x2 = floor(random(w));
+    print(x2);
+    let y2 = floor(random(h));
+  badFood = createVector(x, y);
+   // badFoodX = createVector(x2, y2);
+  
+  }
+  
+  locationTwo(){
+  
+  
+  }
+}
